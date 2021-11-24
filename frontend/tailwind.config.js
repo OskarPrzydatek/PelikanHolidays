f3e1d8dcc5d56d524a7e3e8a438cce9264f4a58b
Oskar <oskar.m.przydatek@gmail.com>
2021-11-24 01:57:33 +0100

    config files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,43 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xs: ".75rem",
+        s: ".875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+        "7xl": "5rem",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      outline: ["focus", "focus-visible"],
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        "*": {
+          boxSizing: "border-box",
+          fontFamily: "Arial Narrow, Arial, sans-serif",
+        },
+      });
+    }),
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
+};
