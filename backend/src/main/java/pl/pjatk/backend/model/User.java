6d8bbe47f2a8fe7c14478985f4332203d8a2eac7
MaciejLeciejewski <56191731+s21484-pj@users.noreply.github.com>
2021-11-27 16:00:39 +0100

    user model

---
+++ backend/src/main/java/pl/pjatk/backend/model/User.java
@@ -0,0 +1,8 @@
+package pl.pjatk.backend.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+    private UserType userType;
+}
---
+++ backend/src/main/java/pl/pjatk/backend/model/UserType.java
@@ -0,0 +1,7 @@
+package pl.pjatk.backend.model;
+
+public enum UserType {
+    ADMIN,
+    MANAGER,
+    WORKER
+}
