eff8bed6789d79d5dce0f79e7599161fa504827a
MaciejLeciejewski <56191731+s21484-pj@users.noreply.github.com>
2021-12-07 20:23:32 +0100

    login

---
+++ backend/src/main/java/pl/pjatk/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pl.pjatk.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pl.pjatk.backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+}
