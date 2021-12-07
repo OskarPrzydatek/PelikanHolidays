922144147ffb16eef08d90569827c45868870936
MaciejLeciejewski <56191731+s21484-pj@users.noreply.github.com>
2021-12-07 20:23:32 +0100

    login

---
+++ backend/src/test/java/pl/pjatk/backend/UserRepositoryTests.java
@@ -0,0 +1,42 @@
+package pl.pjatk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import pl.pjatk.backend.model.User;
+import pl.pjatk.backend.model.UserType;
+import pl.pjatk.backend.repository.UserRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("root@pelikanholidays.com");
+        user.setPassword("root");
+        user.setFirstName("root");
+        user.setLastName("root");
+        user.setUserType(UserType.ADMIN);
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+    }
+}
