08ff0cbba8a7e71dbe20ffe060ef0422107468a5
MaciejLeciejewski <56191731+s21484-pj@users.noreply.github.com>
2022-01-16 01:13:25 +0100

    user crud

--- backend/src/main/java/pl/pjatk/backend/controller/UserController.java
+++ backend/src/main/java/pl/pjatk/backend/controller/UserController.java
@@ -4,21 +4,24 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 import pl.pjatk.backend.model.User;
-import pl.pjatk.backend.repository.UserRepository;
+import pl.pjatk.backend.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/login")
-    public ResponseEntity<User> getUserByEmail(@RequestParam String email, @RequestParam String password) {
-        User user = userRepository.findByEmail(email);
+    public ResponseEntity<User> processLoginByEmailAndPassword(
+            @RequestParam String email, @RequestParam String password) {
+        User user = userService.findUserByEmail(email);
 
         if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
             return ResponseEntity.ok(user);
@@ -27,20 +30,41 @@ public class UserController {
         }
     }
 
-//    @GetMapping("/register")
-//    public String showRegistrationForm(Model model) {
-//        model.addAttribute("user", new User());
-//        return "signup_form";
-//    }
-//
-//    @PostMapping("/process_register")
-//    public String processRegister(User user) {
-//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        String encodedPassword = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(encodedPassword);
-//
-//        userRepository.save(user);
-//
-//        return "register_success";
-//    }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        if (userService.existUserById(id)) {
+            return ResponseEntity.ok(userService.findUserById(id));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.addUser(user));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUserById(@PathVariable Long id, @RequestBody User user) {
+        if (userService.existUserById(id)) {
+            return ResponseEntity.ok(userService.updateUserById(id, user));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        if (userService.existUserById(id)) {
+            userService.deleteUserById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
