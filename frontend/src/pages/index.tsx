0d246c79d061ddd4e6fe75e0e56d907fc9c8ec15
Oskar <oskar.m.przydatek@gmail.com>
2022-01-18 13:54:26 +0100

    loading component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,19 +2,14 @@ import type { NextPage } from "next";
 import LoginPageLayout from "@layouts/LoginPageLayout/LoginPageLayout";
 import CompanyLogo from "@molecules/CompanyLogo/CompanyLogo";
 import LoginForm from "../components/forms/LoginForm/LoginForm";
-import Loading from "@atoms/Loading/Loading";
 
 const Home: NextPage = () => {
   return (
-    
-    <Loading />
+    <LoginPageLayout>
+      <CompanyLogo />
+      <LoginForm />
+    </LoginPageLayout>
   );
 };
 
 export default Home;
-
-
-{/* <LoginPageLayout>
-      <CompanyLogo />
-      <LoginForm />
-    </LoginPageLayout> */}
